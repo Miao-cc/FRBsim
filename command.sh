387173e541b0ec2c0228b8617f66cfccdac6a5a9
Miao-cc <1033570101@qq.com>
2021-10-04 01:55:52 +0800

    simulate script

--- command.sh
+++ command.sh
@@ -1,22 +1,20 @@
-root=/home/wangpei/FRBsim
+root=`pwd`
+
 cd ${root}
+mkdir simBinarydata fastdata
 
-# frb 121102 sim
-
-#cd ${root}/simBinarydata
-#${root}/OPTIMUS/simulateSimplePsr_mcc -p FAST_19Beam.params -p test_DM_300_P0_0.005s.params -o test_DM_300_P0_0.005s.dat
-
-#${root}/OPTIMUS/simulateSimplePsr_mcc -p FAST_19Beam.params -p test_DM_200_P0_0.0007236s.params -o test_DM_200_P0_0.0007236s.dat
-
-#cd ${root}/
-#python simMultiPsr.py simulate-Test.yaml
-#python simMultiPsr.py simulate-Test1.yaml
-
-
-python bandspec.py 1300 1600
+# 
+python bandspec.py 500 2000
+python bandspec.py 500 2000
 python bandspec.py 1300 1700
-python bandspec.py 1300 1500
+python bandspec.py 1300 1600
 python bandspec.py 1400 1600
-python bandspec.py 1400 1700
-python bandspec.py 1200 1600
-python bandspec.py 1200 1300
+
+# 
+ln -s params/FAST_19Beam.params simBinarydata/
+cd ${root}/simBinarydata
+${root}/OPTIMUS/simulateSimplePsr_mcc -p FAST_19Beam.params -p ../params/test_DM_200_W10_0.001.params -o test_DM_200_W10_0.001.dat
+
+# 
+cd ${root}/
+python simMultiPsr.py simulate.yaml
