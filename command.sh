5f7c13df4f6e62b82066fd2ceb2a15c017593976
Miao-cc <1033570101@qq.com>
2021-11-04 22:57:43 +0800

    update the auto run script

--- command.sh
+++ command.sh
@@ -1,20 +1,20 @@
 root=`pwd`
 
+# create the folder
 cd ${root}
 mkdir simBinarydata fastdata
 
-# 
+#  create the band spec
 python bandspec.py 500 2000
 python bandspec.py 500 2000
 python bandspec.py 1300 1700
 python bandspec.py 1300 1600
 python bandspec.py 1400 1600
 
-# 
-ln -s params/FAST_19Beam.params simBinarydata/
+# simulate the single pulse
 cd ${root}/simBinarydata
-${root}/OPTIMUS/simulateSimplePsr_mcc -p FAST_19Beam.params -p ../params/test_DM_200_W10_0.001.params -o test_DM_200_W10_0.001.dat
+${root}/OPTIMUS/simulateSimplePsr_mcc -p ${root}/paramsFAST_19Beam.params -p ${root}/params/test_DM_200_W10_0.001.params -o test_DM_200_W10_0.001.dat
 
-# 
+# simulate the FRB
 cd ${root}/
 python simMultiPsr.py simulate.yaml
